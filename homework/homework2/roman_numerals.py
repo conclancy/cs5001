a0286273c1559032b43f92396187e5b45007f5ed
conclancy <conclancy@hotmail.com>
2022-02-07 21:10:15 -0500

    Gradescope Updates

--- homework/homework2/roman_numerals.py
+++ homework/homework2/roman_numerals.py
@@ -18,62 +18,43 @@ def main(number):
     else:
         roman = ""
         number_remainder = number
-     
-    # 400-999 logic   
-    if number_remainder >= 900:
-        number_remainder -= 900
-        roman += "CM"
-    elif number_remainder >= 500:
+
+    # 500 logic   
+    if number_remainder >= 500:
         number_remainder -= 500
         roman += "D"
-    elif number_remainder >= 400:
-        number_remainder -= 400
-        roman += "CD"
-    
-    # 100 - 399 logic 
+
+    # 100's logic 
     if number_remainder >= 100:
         hundreds_int = number_remainder // 100
         roman += "C" * hundreds_int
         number_remainder = number_remainder - (hundreds_int * 100)
 
-    # 50 - 99 Logic 
-    if number_remainder >= 90:
-        number_remainder -= 90
-        roman += "XC"
-    elif number_remainder >= 50:
+    # 50 Logic 
+    if number_remainder >= 50:
         number_remainder -= 50
         roman += "L"
-    elif number_remainder >= 40:
-        number_remainder -= 40
-        roman += "XL"
-    
-    # 10 - 39 Logic 
+
+    # 10's Logic 
     if number_remainder >= 10:
-        hundreds_int = number_remainder // 10
-        roman += "X" * hundreds_int
-        number_remainder = number_remainder - (hundreds_int * 10)
-    
-    # 5 - 9 Logic 
-    if number_remainder >= 9:
-        number_remainder -= 9
-        roman += "IX"
-    elif number_remainder >= 5:
+        tens_int = number_remainder // 10
+        roman += "X" * tens_int
+        number_remainder = number_remainder - (tens_int * 10)
+
+    # 5 Logic 
+    if number_remainder >= 5:
         number_remainder -= 5
         roman += "V"
-    elif number_remainder >= 4:
-        number_remainder -= 4
-        roman += "IV"
-    
-    # 1 - 3 Logic 
+
+    # 1's Logic 
     if number_remainder >= 1:
-        hundreds_int = number_remainder // 1
-        roman += "I" * hundreds_int
+        ones_int = number_remainder // 1
+        roman += "I" * ones_int
 
     # Print Answer 
-    print(roman)
-    
+    print(number, "is", roman)
+
 
 if __name__ == "__main__":
-    number = int(input("Input first number: "))
+    number = int(input("Enter number:"))
     main(number)
-    
