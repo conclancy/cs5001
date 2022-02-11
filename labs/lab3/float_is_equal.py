ace8f8541ad9707b2bc8ae8a477ce786575efb4e
conclancy <conclancy@hotmail.com>
2022-02-10 19:53:18 -0500

    correct fab logic

--- labs/lab3/float_is_equal.py
+++ labs/lab3/float_is_equal.py
@@ -4,7 +4,6 @@
     08 FEB 22 
 '''
 from math import fabs
-from random import random
 
 def float_is_equal(first_float, second_float, threshold):
     '''
@@ -13,12 +12,32 @@ def float_is_equal(first_float, second_float, threshold):
     returns: bool
     '''
 
-    if fabs(first_float, second_float) < threshold:
+    if fabs(first_float - second_float) < threshold:
         return True
     else:
         return False
 
 def main():
+
+    # instantiate variables 
+    test_passed = 0
+    test_failed = 0
+
+    test_cases =[[1.01,1.01, True], [3.123456,3.123456, True],
+                 [2.329, 2.328, False], [2.0, 2.01, False]]
+    
+    # test the test cases 
+    for t in test_cases:
+        if float_is_equal(t[0], t[1],.000001) == t[2]:
+            test_passed += 1
+        else:
+            test_failed += 1
+    
+    # return final results
+    if test_passed == len(test_cases):
+        print("All test passed!")
+    else:
+        print(test_passed, "tests passed, but", test_failed, "failed :(")
     
 
 main()
