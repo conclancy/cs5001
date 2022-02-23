86be360f7d75bf95787f8218392222d92d809858
conclancy <conclancy@hotmail.com>
2022-02-22 19:22:35 -0500

    Add testing to budget.

--- labs/lab5/within_budget.py
+++ labs/lab5/within_budget.py
@@ -14,7 +14,7 @@ def within_budget(prices_list, budget):
     params: list of floats and a single float
     returns: list of floats
     '''
-    
+
     # create function variables
     i = 0
     budget_list = []
@@ -29,7 +29,7 @@ def within_budget(prices_list, budget):
 
         # increment iterator 
         i += 1
-    
+
     # return the list of items within budget
     return budget_list
 
@@ -49,14 +49,30 @@ def main():
     t[1] = budget
     t[2] = expected outcome
     '''
-    test_list = [[[1, 2, 3], 2, [1, 2]]]
+    test_list = [[[1, 2, 3], 2, [1, 2]],
+                 [[-1, 3, 8], 5, [-1, 3]],
+                 [[1.5, 3, 3.5], 3.5, [1.5, 3, 3.5]],
+                 [[.5, .49, .499], .49, [.49]],
+                 [[1, 2, 3], .5, []],
+                 [[10, 20, 30], 0, []]]
 
+    # iterate through tests 
     while i < len(test_list):
+
+        # record tests that pass and fail based on expected outcomes
         if within_budget(test_list[i][0], test_list[i][1]) == test_list[i][2]:
             tests_passed += 1
-        
+        else: 
+            tests_failed += 1
+
+        # increment iterator 
         i += 1
 
-    print(tests_passed)
+    # print final test results
+    if tests_failed != 0:
+        print(tests_failed, "tests failed!  Check code or test cases")
+    else:
+        print("All tests passed!")
+
 
 main()
