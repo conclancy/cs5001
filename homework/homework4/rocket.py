ec026f91744911886321b2e610bf3156e47e9619
Connor Clancy <cclancy@wayfair.com>
2022-03-09 12:56:34 -0500

    Initial rocket commit

---
+++ homework/homework4/rocket.py
@@ -0,0 +1,26 @@
+def down_cone(size):
+    '''
+    Creates the downward facing code \/ 
+    params: single int size
+    returns: multi-line string
+    '''
+
+    cone_string = ""
+
+    while s <= size:
+        print()
+
+
+def rocket(size): 
+    '''
+    This function exxecutes the rest of the code required to print the rocket
+    params: single int size
+    returns: multi-line string 
+    '''
+
+    print(size)
+
+def main():
+    rocket(3)
+
+main()
