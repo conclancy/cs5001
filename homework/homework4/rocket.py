f4f1b8691c8a86b25f2476bed3c9973c68bf3b62
conclancy <conclancy@hotmail.com>
2022-03-09 19:44:20 -0500

    Add up_cone function

--- homework/homework4/rocket.py
+++ homework/homework4/rocket.py
@@ -30,6 +30,39 @@ def down_cone(size):
     return cone_string
 
 
+def up_cone(size):
+    '''
+    Creates the upward facing code /\ 
+    params: single int size
+    returns: multi-line string
+    '''
+
+    # function constants
+    CONE_SHAPE = "/\\"
+
+    # funtion variables
+    cone_string = ""
+    dot_count = size - 1
+    cone_count = 1
+
+    # generate code string
+    while cone_count <= size: 
+
+        # generate dot and cone string
+        loop_string = "|" + (dot_count * ".") + (cone_count * CONE_SHAPE) + (
+                      dot_count * "." * 2) + (cone_count * CONE_SHAPE) + (
+                      dot_count * ".") + "|" + "\n"
+
+        # add row rocket part 
+        cone_string = cone_string + loop_string
+
+        # update iteration variables
+        cone_count = cone_count + 1
+        dot_count = dot_count - 1
+
+    return cone_string
+
+
 def rocket(size): 
     '''
     This function exxecutes the rest of the code required to print the rocket
@@ -47,10 +80,13 @@ def rocket(size):
     # first down cone 
     rocket = rocket + down_cone(size)
 
+    # first up cone
+    rocket = rocket + up_cone(size)
+
     print(rocket)
 
 
 def main():
-    rocket(3)
+    rocket(4)
 
 main()
