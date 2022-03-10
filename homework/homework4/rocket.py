01a69cab3bceb7bd0b0ad2942a85d49b2f7180a1
conclancy <conclancy@hotmail.com>
2022-03-09 19:37:40 -0500

    Add first down cone section

--- homework/homework4/rocket.py
+++ homework/homework4/rocket.py
@@ -5,10 +5,29 @@ def down_cone(size):
     returns: multi-line string
     '''
 
-    cone_string = ""
+    # function constants
+    CONE_SHAPE = "\\/"
 
-    while s <= size:
-        print()
+    # funtion variables
+    cone_string = ""
+    dot_count = 0
+
+    # generates cone string 
+    while size > 0:  
+
+        # generate dot and cone row
+        loop_string = "|" + (dot_count * ".") + (size * CONE_SHAPE) + (
+                      dot_count * "." * 2) + (size * CONE_SHAPE) + (
+                      dot_count * ".") + "|" + "\n"
+
+        # add row rocket part 
+        cone_string = cone_string + loop_string
+
+        # update iteration variables
+        size = size - 1
+        dot_count = dot_count + 1
+
+    return cone_string
 
 
 def rocket(size): 
@@ -18,7 +37,18 @@ def rocket(size):
     returns: multi-line string 
     '''
 
-    print(size)
+    # function variables
+    rocket = ""
+
+    # error handling logic
+    if size < 3:
+        return("Rocket sizes must be at least 3")
+
+    # first down cone 
+    rocket = rocket + down_cone(size)
+
+    print(rocket)
+
 
 def main():
     rocket(3)
