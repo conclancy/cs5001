07f33c007de878201fa100e5497de04eda798739
conclancy <conclancy@hotmail.com>
2022-04-16 17:35:26 -0400

    Word object passing all gradescope tests

--- labs/lab11/word.py
+++ labs/lab11/word.py
@@ -13,6 +13,12 @@ from stack import Stack
 
 
 class Word:
+    '''
+    The Word class represents a word in the form of a string
+    attributes: input_word
+    methods: populate_stack, popuate_queue, split_string, palindrome_test,
+             is_palindrome, is_repeat
+    '''
 
     def __init__(self, input_word):
         '''initialize the word class'''
@@ -21,6 +27,7 @@ class Word:
         self.word_stack = Stack()
         self.word_queue = Queue()
 
+        # ensure the input_word variable is a single word string
         if isinstance(input_word, str):
             if ' ' not in input_word:
                 self.input_word = input_word.lower()
@@ -68,16 +75,52 @@ class Word:
         for i in word:
             queue.enqueue(i)
 
+    def split_string(self, queue, number_of_times) -> None:
+        '''
+        split_string takes in a queue and number variable and splits the
+        element in the queue the number of times specified by the user. 
+        params:
+            self -- the current object
+            queue -- the queue object containing a single string
+            number_of_times -- int for the number of times to be split
+        returns:
+            void
+        '''
+
+        # cycle through the queue and cut the elements in half the
+        # number of times specifid by the input
+        for r in range(1, number_of_times + 1):
+            for p in range(1, r):
+                partial_string = queue.dequeue()
+                middle_index = int(len(partial_string) / 2)
+                queue.enqueue(partial_string[:middle_index])
+                queue.enqueue(partial_string[middle_index:])
+
     def palindrome_test(self, test_word) -> bool:
+        '''
+        palindrome_test returns a boolean representing whether the input
+        word is a palindrome
+        params:
+            self -- the current object4
+            test_word -- the string being tested as a palindrome
+        return:
+            bool
+        '''
+
         # create variable to return
         palindrome_flag = True
 
+        # instantiate and populate a stack with the letters of the word
         test_stack = Stack()
         self.populate_stack(test_word, test_stack)
 
+        # instantiate and populate a queue with the letters of the word
         test_queue = Queue()
         self.populate_queue(test_word, test_queue)
 
+        # for each letter in the word test if the next letter from 
+        # the stac and queue match.  Set the palindrome_flag to false 
+        # if one or more of the letters does not match
         for letter in test_word:
             if test_stack.pop() != test_queue.dequeue():
                 palindrome_flag = False
@@ -95,14 +138,19 @@ class Word:
             bool
         '''
 
+        # test to ensure number_of_times is of type int
         if not isinstance(number_of_times, int):
             raise ValueError("number_of_times must be of type int")
 
+        # if number_of_times is one, run the palindrome test directly on
+        # the input_word
         if number_of_times == 1:
 
             # test to see if the word is a palindrome
             return self.palindrome_test(self.input_word)
 
+        # if number_of_times is greater than one, split the word up before
+        # running the palindrome tests
         elif number_of_times > 1: 
 
             # all nested palindromes will have an even number of elements. 
@@ -114,39 +162,108 @@ class Word:
                 return False
             else:
                 # create a queue to hold to strings for processing
-                q = Queue()
+                queue = Queue()
 
                 # add the input word to the queue
-                q.enqueue(self.input_word)
+                queue.enqueue(self.input_word)
 
-                # cycle through the queue and cut the elements in half the
-                # number of times specifid by the input
-                for r in range(1, number_of_times + 1):
-                    for p in range(1, r):
-                        partial_string = q.dequeue()
-                        middle_index = int(len(partial_string) / 2)
-                        q.enqueue(partial_string[:middle_index])
-                        q.enqueue(partial_string[middle_index:])
+                # split the queue into substrings
+                self.split_string(queue, number_of_times)
 
                 # test each of the elements in the queue to ensure it is a 
                 # palindrome.  Return False if it is not
                 for r in range(number_of_times):
-                    if not self.palindrome_test(q.dequeue()):
+                    if not self.palindrome_test(queue.dequeue()):
                         return False
 
                 return True
 
+        # raise an error if number_of_times is less than 1
+        else:
+            err = "number_of_times must be greater than or equal to 1"
+            raise ValueError(err)
+
+    def is_repeat(self, number_of_times=1) -> bool:
+        '''
+        is_repeat tests if a string contains the same repeated the
+        specified number of times
+        params:
+            self -- the current object
+            number_of_times -- int representing the number of tests
+        returns:
+            bool
+        '''
+
+        # test to ensure the input number is an int
+        if not isinstance(number_of_times, int):
+            raise ValueError("number_of_times must be of type int")
+
+        # For the purposes of this function, if number_of_times = 1, then
+        # the word is considered a reapeat. 
+        if number_of_times == 1:
+            return True
+
+        # If number_of_times > 1 then run throught the following logic to 
+        # test the input string
+        elif number_of_times > 1: 
+
+            # create a queue and stack to hold to strings for processing
+            queue = Queue()
+            previous_word = Stack()
+
+            # add the input word to the queue
+            queue.enqueue(self.input_word)
+            word_length = int(len(self.input_word) / number_of_times)
+
+            # cycle through the queue and cut the elements in half the
+            # number of times specifid by the input
+            for r in range(1, number_of_times + 1):
+                partial_string = queue.dequeue()
+
+                if len(partial_string) <= word_length:
+                    queue.enqueue(partial_string)
+                else:
+                    queue.enqueue(partial_string[word_length:])
+                    queue.enqueue(partial_string[:word_length])
+
+            # test each of the elements in the queue to ensure it is a 
+            # repeat of the previous word.  Return False if it is not.
+            for r in range(number_of_times):
+
+                # set the element for the first iteration
+                if r == 0:
+                    previous_word.push(queue.dequeue())
+
+                # compare the previous string and current string.
+                # return false if they are not the same word
+                else:
+                    current_word = queue.dequeue()
+                    if previous_word.pop() != current_word:
+                        return False 
+                    else:
+                        previous_word.push(current_word) 
+
+            return True
+
+        # raise an error if number_of_times is less than 1
         else:
             err = "number_of_times must be greater than or equal to 1"
             raise ValueError(err)
 
 
 if __name__ == "__main__":
+    '''Basic tests to run while developing the code'''
+
     r = Word("racecar")
     print(r.is_palindrome(), "racecar is a palindrome")
+    print(r.is_palindrome(), "racecar is a repeated")
 
     c = Word("Connor")
     print(c.is_palindrome(), "connor is not a palindrome")
 
     c = Word("racecarracecarracecarracecar")
     print(c.is_palindrome(3), "racecarracecar is a palindrome")
+    # print(c.is_repeat(3), "racecarracecar is a repeat")
+
+    c = Word("jmgukndhemmehdnkugmjjmgukndhemmehdnkugmjjmgukndhemmehdnkugmj")
+    print(c.is_repeat(3), "jmg is a repeat")
